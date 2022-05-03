c0b6c6e73081e7cfc237aa05640102d4b162cb2a
Giovanni Ciotta <giociotta@gmail.com>
2022-05-03 16:23:32 +0200

    nome -cognome-colore

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+
+
+//  chiediamo il nome 
 const nome = prompt("Come ti chiami?")
 
-document.getElementById("presentazione").innerHTML= `Ciao ${nome}`; 
+document.getElementById("nome").innerHTML= `Ciao ${nome}`; 
+console.log(nome)
+
+
+// chiediamo il cognome 
+const cognome = prompt(" Cognome?")
+
+document.getElementById("cognome").innerHTML= `  ${cognome}`;
+
+console.log(cognome)
+
+// chiediamo il suo colore preferito 
+const colore = prompt(" Il tuo colore preferito?")
+
+document.getElementById("colore").innerHTML= ` Il tuo colore preferito è: ${colore}`; 
+console.log(colore)
